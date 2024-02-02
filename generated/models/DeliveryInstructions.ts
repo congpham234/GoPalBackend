eb6451d52f4884631e34826256430437f3846647
Cong pham <>
2024-02-02 01:51:08 -0800

    Remove generated folder

--- generated/models/DeliveryInstructions.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* eslint-disable */
-export enum DeliveryInstructions {
-    LEAVE_AT_FRONT_DOOR = 'LeaveAtFrontDoor',
-    LEAVE_AT_BACK_DOOR = 'LeaveAtBackDoor',
-    OTHER = 'Other',
-}
