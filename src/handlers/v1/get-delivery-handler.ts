de3fd92e481ff051fd76d5a712fc222b6d90a9e5
Cong pham <>
2024-04-29 22:27:09 -0700

    reverted the change that used for testing

--- src/handlers/v1/get-delivery-handler.ts
+++ src/handlers/v1/get-delivery-handler.ts
@@ -12,11 +12,10 @@ export class GetDeliveryHandler {
   }
 
   public async getDelivery(deliveryId: string): Promise<Delivery> {
-    throw new Error("CONG IS HERE");
     const delivery = await this.deliveriesDao.getDeliveryById(deliveryId);
     if (delivery) {
       // TODO: write better mapping between internal objects vs client object
-      // return delivery;
+      return delivery;
     } else {
       throw new DeliveryNotFoundError('Delivery not found');
     }
